721cc592a88af7d1274ffec57e4a3d4c13210324
MyCatNattles <native.neapolitan@gmail.com>
2016-11-27 20:29:04 -0500

    Details processor changes

---
+++ details_processor.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Volunteer tasks</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="style.css" rel="stylesheet">
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	</head>
+	<body>
+		<header>
+			<img src="helpinghands.png" alt="Helping Hands">
+		</header>
+		<nav>
+			<?php require_once 'inc/nav.php'; ?>
+		</nav>
+		<main>
+			<?php
+				require_once('inc/db_connect.php');
+			
+				if(isset($_SESSION['id']))
+				{
+					$user_id = $_SESSION['id'];
+					$task_id = $_POST['task_id'];
+					$enrolled = $_POST['enrolled'];
+				}
+				else
+				{
+					echo 'Error: No user logged in';
+				}
+			?>	
+					
+			<?php
+			
+			//If $enrolled == 1, then the user is removing themselves. Else, the user is signing up.
+
+			if ($enrolled == 1) {
+			
+			try {
+				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+			// set the PDO error mode to exception
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			// sql to delete a record
+				$sql = "DELETE FROM user_task WHERE user_id= $user_id";
+
+			// use exec() because no results are returned
+				$conn->exec($sql);
+				echo "Sorry to see you go!";
+				}
+				catch(PDOException $e)
+				{
+				echo $sql . "<br>" . $e->getMessage();
+				}
+
+			$conn = null;
+			
+			} else {
+			
+			try {
+				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			
+			// set the PDO error mode to exception
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$sql = "INSERT INTO user_task (user_id, task_id)
+				VALUES ('$user_id', '$task_id')";
+			// use exec() because no results are returned
+				$conn->exec($sql);
+				echo "Thanks for signing up!";
+		}
+			catch(PDOException $e)
+		{
+			echo $sql . "<br>" . $e->getMessage();
+		}
+			$conn = null;
+
+		}
+			
+			?>
+			
+		</main>
+	</body>
+</html>
+
